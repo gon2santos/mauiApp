bd19e20026c3acd202060b4cacac96dc4025622d
gon2santos <necchigonzalo@gmail.com>
2023-06-16 21:42:02 -0300

    add contacts list as ObservableCollection

--- Views/Contacts.xaml.cs
+++ Views/Contacts.xaml.cs
@@ -2,25 +2,32 @@ namespace MauiApp1.Views;
 
 using Contact = MauiApp1.Models.Contact;
 using ContactRepository = MauiApp1.Models.ContactRepository;
+using System.Collections.ObjectModel;
 
 public partial class Contacts : ContentPage
 {
-	public Contacts()
-	{
-		InitializeComponent();
+    public Contacts()
+    {
+        InitializeComponent();
+    }
 
-		List<Contact> contactNames = ContactRepository.GetAllContacts();
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
 
-		listContacts.ItemsSource = contactNames;
-	}
+        var contactNames = new ObservableCollection<Contact>(ContactRepository.GetAllContacts());
+
+        listContacts.ItemsSource = contactNames;
+    }
 
     private async void listContacts_ItemSelected(object sender, SelectedItemChangedEventArgs e)
     {
-		if (listContacts.SelectedItem != null) {
-			//logic
-			await Shell.Current.GoToAsync($"{nameof(EditContact)}?Id={((Contact)listContacts.SelectedItem).ContactId}");
-		}
-		
+        if (listContacts.SelectedItem != null)
+        {
+            //logic
+            await Shell.Current.GoToAsync($"{nameof(EditContact)}?Id={((Contact)listContacts.SelectedItem).ContactId}");
+        }
+
     }
 
     private void listContacts_ItemTapped(object sender, ItemTappedEventArgs e)
